d1aadeb71d69a4ade8a4d9aad0766284f724d744
Deviron <samdeviron@gmail.com>
2021-10-07 20:28:15 +0200

    add library packages for python-ldap

--- kali-setup-script.sh
+++ kali-setup-script.sh
@@ -34,7 +34,7 @@ apt update
 apt upgrade -y
 apt dist-upgrade -y
 
-apt install vim build-essential apt-transport-https axel cupp kali-desktop-gnome -y
+apt install vim build-essential apt-transport-https axel libsasl2-dev python-dev libldap2-dev libssl-dev kali-desktop-gnome -y
 
 apt autoremove
 
