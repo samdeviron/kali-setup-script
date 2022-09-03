9b033d6883901d1b87aa8724157d56eddd66a050
Sam Deviron <80849478+samdeviron@users.noreply.github.com>
2022-09-03 20:11:42 +0200

    Update kali-setup-script.sh

--- kali-setup-script.sh
+++ kali-setup-script.sh
@@ -18,94 +18,136 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# Ask the user for login details
-echo "Set your desired username and password"
-read -p 'Username: ' uservar
+create_user() {
+   # Ask the user for login details
+   echo "Set your desired username and password"
+   read -p 'Username: ' uservar
 
-killall -u kali
+   killall -u kali
 
-usermod -l $uservar kali
-usermod -m -d /home/$uservar $uservar
+   usermod -l $uservar kali
+   usermod -m -d /home/$uservar $uservar
 
-passwd $uservar
+   passwd $uservar
 
-sed -i "s/Kali/${uservar}/" /etc/passwd
+   sed -i "s/Kali/${uservar}/" /etc/passwd
+}
 
-# Run updates
-apt update
-apt upgrade -y
-apt dist-upgrade -y
 
-apt install vim build-essential apt-transport-https axel libsasl2-dev seclists gobuster python2-dev libldap2-dev libssl-dev kali-desktop-gnome terminator flameshot linux-headers-$(uname -r) -y
 
-apt autoremove
+install_essentials() {
+   # Run updates
+   apt update
+   apt upgrade -y
+   apt dist-upgrade -y
 
-# Install tmux logging
-runuser -l $uservar -c "cd /opt && mkdir tmux-logging"
-chown -R $uservar: /opt
-git clone https://github.com/tmux-plugins/tmux-logging.git /opt/tmux-logging
+   apt install vim build-essential apt-transport-https axel libsasl2-dev seclists gobuster python2-dev libldap2-dev libssl-dev kali-desktop-gnome terminator flameshot linux-headers-$(uname -r) -y
 
-# Install Sublime Text
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-apt update
-apt install sublime-text -y
+   apt autoremove
+}
 
-# Install Joplin & Google Chrome
-runuser -l $uservar -c "wget -O - https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh | bash"
-runuser -l $uservar -c "cd /home/${uservar}/Downloads && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+install_tmux() {
+   # Install tmux logging
+   runuser -l $uservar -c "cd /opt && mkdir tmux-logging"
+   chown -R $uservar: /opt
+   git clone https://github.com/tmux-plugins/tmux-logging.git /opt/tmux-logging
+   runuser -l $uservar -c "cd /home/${uservar} && wget https://raw.githubusercontent.com/samdeviron/tmux.conf/main/.tmux.conf"
+}
 
-apt install /home/$uservar/Downloads/google-chrome-stable_current_amd64.deb -y
+install_sublime() {
+   # Install Sublime Text
+   wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+   echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+   apt update
+   apt install sublime-text -y
+}
 
-# Install /opt tools
-runuser -l $uservar -c "cd /opt"
-cd /opt
-runuser -l $uservar -c "git clone https://github.com/AonCyberLabs/Windows-Exploit-Suggester.git /opt/windows-exploit-suggester"
-runuser -l $uservar -c "git clone https://github.com/bitsadmin/wesng.git /opt/wesng"
-runuser -l $uservar -c "git clone https://github.com/galkan/crowbar.git /opt/crowbar"
-runuser -l $uservar -c "git clone https://github.com/BC-SECURITY/Empire.git /opt/empire"
-runuser -l $uservar -c "git clone https://github.com/ropnop/windapsearch.git /opt/windapsearch" 
-runuser -l $uservar -c "pip install python-ldap" 
-runuser -l $uservar -c "mkdir /opt/tunnel && git clone https://github.com/sensepost/reGeorg.git /opt/tunnel/reGeorg" 
+install_joplin() {
+   # Install Joplin
+   runuser -l $uservar -c "wget -O - https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh | bash"
+}
 
-# Install Impacket
-apt install python3-pip -y
-runuser -l $uservar -c "git clone https://github.com/SecureAuthCorp/impacket.git /opt/impacket"
-pip3 install -r /opt/impacket/requirements.txt
-cd /opt/impacket && python3 /opt/impacket/setup.py install
+install_chrome() {
+   # Install Google Chrome
+   runuser -l $uservar -c "cd /home/${uservar}/Downloads && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 
-# Install latest burp suite
-runuser -l $uservar -c "cd /home/${uservar}/Downloads && curl --header 'Host: portswigger.net' --user-agent 'Mozilla/5.0 (X11; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0' --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' --header 'Accept-Language: en-US,en;q=0.5' --referer 'https://portswigger.net/burp/releases/professional-community-2021-8-4' --cookie 'SessionId=CfDJ8E%2Bwz0j835xPn7BOifDmT9Ll3pv0cz33oybtmJQgyPs4JVEBAgLDF0DHOaNQiHWWZfwy6gOSv8eJUg43TWm%2FzD6j459errefShcsjLiU7%2Bylxn%2B97nxa%2B3lR8YNKGFVRJkzOSnW6Qqy0aCIncFnjabmVrGtp1kqCMOz2hkiAIfVc' --header 'Upgrade-Insecure-Requests: 1' --header 'Sec-Fetch-Dest: document' --header 'Sec-Fetch-Mode: navigate' --header 'Sec-Fetch-Site: same-origin' --header 'Sec-Fetch-User: ?1' 'https://portswigger.net/burp/releases/download?product=community&version=2021.8.4&type=Linux' --output 'burpsuite_community_linux_v2021_8_4.sh'"
-runuser -l $uservar -c "chmod +x /home/${uservar}/Downloads/burpsuite_community_linux_v2021_8_4.sh && /home/${uservar}/Downloads/burpsuite_community_linux_v2021_8_4.sh"
+   apt install /home/$uservar/Downloads/google-chrome-stable_current_amd64.deb -y
+}
 
-runuser -l $uservar -c "cd /home/${uservar} && wget https://raw.githubusercontent.com/samdeviron/tmux.conf/main/.tmux.conf"
+install_opt() {
+   # Install /opt tools
+   runuser -l $uservar -c "cd /opt"
+   cd /opt
+   runuser -l $uservar -c "git clone https://github.com/AonCyberLabs/Windows-Exploit-Suggester.git /opt/windows-exploit-suggester"
+   runuser -l $uservar -c "git clone https://github.com/bitsadmin/wesng.git /opt/wesng"
+   runuser -l $uservar -c "git clone https://github.com/galkan/crowbar.git /opt/crowbar"
+   runuser -l $uservar -c "git clone https://github.com/BC-SECURITY/Empire.git /opt/empire"
+   runuser -l $uservar -c "git clone https://github.com/ropnop/windapsearch.git /opt/windapsearch" 
+   runuser -l $uservar -c "pip install python-ldap" 
+   runuser -l $uservar -c "mkdir /opt/tunnel && git clone https://github.com/sensepost/reGeorg.git /opt/tunnel/reGeorg" 
+}
 
-# Install VSCode
+install_impacket() {
+   # Install Impacket
+   apt install python3-pip -y
+   runuser -l $uservar -c "git clone https://github.com/SecureAuthCorp/impacket.git /opt/impacket"
+   pip3 install -r /opt/impacket/requirements.txt
+   cd /opt/impacket && python3 /opt/impacket/setup.py install
+}
 
-runuser -l $uservar -c "cd /home/${uservar}/Downloads && wget https://az764295.vo.msecnd.net/stable/784b0177c56c607789f9638da7b6bf3230d47a8c/code_1.71.0-1662018389_amd64.deb"
-cd /home/${uservar}/Downloads
-apt install ./code_* -y
+install_burp() {
+   # Install latest burp suite
+   runuser -l $uservar -c "cd /home/${uservar}/Downloads && curl --header 'Host: portswigger.net' --user-agent 'Mozilla/5.0 (X11; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0' --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' --header 'Accept-Language: en-US,en;q=0.5' --referer 'https://portswigger.net/burp/releases/professional-community-2021-8-4' --cookie 'SessionId=CfDJ8E%2Bwz0j835xPn7BOifDmT9Ll3pv0cz33oybtmJQgyPs4JVEBAgLDF0DHOaNQiHWWZfwy6gOSv8eJUg43TWm%2FzD6j459errefShcsjLiU7%2Bylxn%2B97nxa%2B3lR8YNKGFVRJkzOSnW6Qqy0aCIncFnjabmVrGtp1kqCMOz2hkiAIfVc' --header 'Upgrade-Insecure-Requests: 1' --header 'Sec-Fetch-Dest: document' --header 'Sec-Fetch-Mode: navigate' --header 'Sec-Fetch-Site: same-origin' --header 'Sec-Fetch-User: ?1' 'https://portswigger.net/burp/releases/download?product=community&version=2021.8.4&type=Linux' --output 'burpsuite_community_linux_v2021_8_4.sh'"
+   runuser -l $uservar -c "chmod +x /home/${uservar}/Downloads/burpsuite_community_linux_v2021_8_4.sh && /home/${uservar}/Downloads/burpsuite_community_linux_v2021_8_4.sh"
+}
 
-# Install Pcloud
-#runuser -l $uservar -c "mkdir /home/${uservar}/Applications"
-#runuser -l $uservar -c "cd /home/${uservar}/Applications && wget https://p-lux3.pcloud.com/cBZsfshJnZQwH1ExZZZubbov7Z2ZZH30ZkZkkypVZBkZapZFRZtzZQzZ3zZrkZ2zZc7ZWRZsFZTJZd5ZY7Z0FAtXZ8P4pu9lCmV0v3vR8qRq6vRVi1OzX/pcloud"
-#runuser -l $uservar -c "chmod +x /home/${uservar}/Applications/pcloud && /home/${uservar}/Applications/pcloud"
+install_vscode() {
+   # Install VSCode
+   runuser -l $uservar -c "cd /home/${uservar}/Downloads && wget https://az764295.vo.msecnd.net/stable/784b0177c56c607789f9638da7b6bf3230d47a8c/code_1.71.0-1662018389_amd64.deb"
+   cd /home/${uservar}/Downloads
+   apt install ./code_* -y
+}
 
-# Set .bashrc settings
-echo "alias ls='ls -lha'" >> /home/${uservar}/.bashrc
-echo "export HISTCONTROL=ignoredups" >> /home/${uservar}/.bashrc
-echo "export HISTIGNORE='&:ls:[bf]g:exit:history'" >> /home/${uservar}/.bashrc
-echo "alias empire-server='cd /opt/empire && /opt/empire/ps-empire server'" >> /home/${uservar}/.bashrc
-echo "alias empire-client='cd /opt/empire && /opt/empire/ps-empire client'" >> /home/${uservar}/.bashrc
-echo "export PATH='/opt/tunnel/reGeorg:/opt/impacket/impacket:/opt/empire:/opt/windapsearch:/opt/windows-exploit-suggester:/opt/wes-ng:$PATH'" >> /home/${uservar}/.bashrc
+install_pcloud() {
+   # Install Pcloud
+   runuser -l $uservar -c "mkdir /home/${uservar}/Applications"
+   runuser -l $uservar -c "cd /home/${uservar}/Applications && wget https://p-lux3.pcloud.com/cBZsfshJnZQwH1ExZZZubbov7Z2ZZH30ZkZkkypVZBkZapZFRZtzZQzZ3zZrkZ2zZc7ZWRZsFZTJZd5ZY7Z0FAtXZ8P4pu9lCmV0v3vR8qRq6vRVi1OzX/pcloud"
+   runuser -l $uservar -c "chmod +x /home/${uservar}/Applications/pcloud && /home/${uservar}/Applications/pcloud"
+}
 
-# Set .zshrc settings
-echo "alias ls='ls -lha'" >> /home/${uservar}/.zshrc
-echo "export HISTCONTROL=ignoredups" >> /home/${uservar}/.zshrc
-echo "export HISTIGNORE='&:ls:[bf]g:exit:history'" >> /home/${uservar}/.zshrc
-echo "alias empire-server='cd /opt/empire && /opt/empire/ps-empire server'" >> /home/${uservar}/.zshrc
-echo "alias empire-client='cd /opt/empire && /opt/empire/ps-empire client'" >> /home/${uservar}/.zshrc
-echo "export PATH='/opt/tunnel/reGeorg:/opt/impacket/impacket:/opt/empire:/opt/windapsearch:/opt/windows-exploit-suggester:/opt/wes-ng:$PATH'" >> /home/${uservar}/.zshrc
+set_bashrc() {
+   # Set .bashrc settings
+   echo "alias ls='ls -lha'" >> /home/${uservar}/.bashrc
+   echo "export HISTCONTROL=ignoredups" >> /home/${uservar}/.bashrc
+   echo "export HISTIGNORE='&:ls:[bf]g:exit:history'" >> /home/${uservar}/.bashrc
+   echo "alias empire-server='cd /opt/empire && /opt/empire/ps-empire server'" >> /home/${uservar}/.bashrc
+   echo "alias empire-client='cd /opt/empire && /opt/empire/ps-empire client'" >> /home/${uservar}/.bashrc
+   echo "export PATH='/opt/tunnel/reGeorg:/opt/impacket/impacket:/opt/empire:/opt/windapsearch:/opt/windows-exploit-suggester:/opt/wes-ng:$PATH'" >> /home/${uservar}/.bashrc
+}
+
+set_zshrc() {
+   # Set .zshrc settings
+   echo "alias ls='ls -lha'" >> /home/${uservar}/.zshrc
+   echo "export HISTCONTROL=ignoredups" >> /home/${uservar}/.zshrc
+   echo "export HISTIGNORE='&:ls:[bf]g:exit:history'" >> /home/${uservar}/.zshrc
+   echo "alias empire-server='cd /opt/empire && /opt/empire/ps-empire server'" >> /home/${uservar}/.zshrc
+   echo "alias empire-client='cd /opt/empire && /opt/empire/ps-empire client'" >> /home/${uservar}/.zshrc
+   echo "export PATH='/opt/tunnel/reGeorg:/opt/impacket/impacket:/opt/empire:/opt/windapsearch:/opt/windows-exploit-suggester:/opt/wes-ng:$PATH'" >> /home/${uservar}/.zshrc
+}
+
+
+create_user()
+install_essentials()
+install_tmux()
+install_sublime()
+install_joplin()
+install_chrome()
+install_opt()
+install_burp()
+install_vscode()
+#install_pcloud()
+set_bashrc()
+set_zshrc()
 
 touch /home/${uservar}/.hushlogin
 
